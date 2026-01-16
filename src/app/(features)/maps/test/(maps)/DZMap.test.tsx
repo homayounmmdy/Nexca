8d65d2678c2ac0e95a2e05a5c0e5f8920159c4aa
Homayoun <homayoun763@gmail.com>
2026-01-16 18:11:01 +0330

    test : DZMaps and refactor it

---
+++ src/app/(features)/maps/test/(maps)/DZMap.test.tsx
@@ -0,0 +1,27 @@
+import { DZ_MAP_DATA } from '../../../../../config/maps';
+import { render, screen } from '@testing-library/react';
+import { expect } from 'vitest';
+import React from 'react';
+import DZMap from '../../(maps)/DZMap';
+
+describe('Andorra Map', () => {
+   const activeProvinceId = '4';
+   it.each(DZ_MAP_DATA)('all province should valid items', ({ secid }) => {
+      const provinceID = `province-${secid}`;
+
+      render(<DZMap setActiveProvinceId={activeProvinceId} />);
+
+      const id = screen.getByTestId(provinceID);
+
+      expect(id).toHaveClass('sm_state provinceSec');
+      expect(id).toHaveAttribute('id', provinceID);
+   });
+
+   it('should have different style for active province', () => {
+      render(<DZMap setActiveProvinceId={activeProvinceId} />);
+
+      const id = screen.getByTestId(`province-${activeProvinceId}`);
+
+      expect(id).toHaveClass('activeProvince');
+   });
+});
