af923275a5d322276af394245ed7cf6314522141
Homayoun <homayoun763@gmail.com>
2026-02-05 20:04:59 +0330

    test : section api

--- src/models/ConnetDB.ts
+++ src/models/ConnetDB.ts
@@ -10,5 +10,9 @@ import mongoose from 'mongoose';
  * Also sets Mongoose's Promise implementation to use the global Promise object.
  * @throws {Error} If the connection to the database fails.
  */
-mongoose.connect(process.env.MONGODB_URI as string);
-mongoose.Promise = global.Promise;
+
+// Only connect if not in test mode
+if (process.env.NODE_ENV !== 'test') {
+   mongoose.connect(process.env.MONGODB_URI as string);
+   mongoose.Promise = global.Promise;
+}
