43ca313f13620a59f3d287541f9e0fea3f918a2d
Homayoun <homayoun763@gmail.com>
2026-02-05 21:31:42 +0330

    test : maps API and provide jsdocs for that too

--- src/app/api/maps/route.ts
+++ src/app/api/maps/route.ts
@@ -2,11 +2,24 @@ import RequestHandler from '@/util/handler/RequestHandler';
 import MapsCash from '@/cash/MapsCash';
 import { MapsModel } from '@/models';
 
+/**
+ * GET /api/maps
+ *
+ * Retrieves a list of all map entries.
+ *
+ */
 export async function GET() {
    const handler = new RequestHandler(MapsModel, MapsCash);
    return handler.GetAll();
 }
 
+/**
+ * POST /api/maps
+ *
+ * Creates a new map entry from the request body.
+ *
+ * @param {Request} req - The incoming HTTP request containing the map data.
+ */
 export async function POST(req: Request) {
    const handler = new RequestHandler(MapsModel, MapsCash);
    return handler.Post(req, 'Map Created successfully');
